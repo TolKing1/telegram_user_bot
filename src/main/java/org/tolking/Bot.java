f6d604057339326b7aafc291905a1af7df1c8527
Tolek <tolking0101@gmail.com>
2023-09-02 15:15:22 +0500

    made more compact and some variables were changed from private to package private in order to get acces from another class

--- src/main/java/org/tolking/Bot.java
+++ src/main/java/org/tolking/Bot.java
@@ -31,9 +31,9 @@ public final class Bot {
     private static final int API_ID = Integer.parseInt(config.getProperty("api_id"));
     private static final String HASH_CODE = config.getProperty("api_hash");
     private static final String PHONE_NUMBER = config.getProperty("phone_number");
-    private static final long USER_ID = Long.parseLong(config.getProperty("user_id"));
+    static final long USER_ID = Long.parseLong(config.getProperty("user_id"));
     private static final long ADMIN_ID = Long.parseLong(config.getProperty("admin_id"));
-    private static SimpleTelegramClient client;
+    static SimpleTelegramClient client;
 
     public static void main(String[] args) throws Exception {
         Init.init();
@@ -87,11 +87,7 @@ public final class Bot {
         MessageContent messageContent = update.message.content;
         // If message is text
         if (messageContent instanceof TdApi.MessageText messageText) {
-            //YouTube Link regex
-            String linkRegex = "^(?:(?:https|http):\\/\\/)?(?:www\\.)?(youtu.*be.*)\\/(watch\\?v=|embed\\/|v|shorts|)(.*?((?=[&#?])|$))";
-            String shortsRegex = "^(?:(?:https|http):\\/\\/)?(?:www\\.)?(youtube.*com.*)\\/(shorts)(.*?((?=[&#?])|$))";
             String text = messageText.text.text;
-            long senderUserId = update.message.senderId instanceof TdApi.MessageSenderUser ? ((TdApi.MessageSenderUser) update.message.senderId).userId : 0;
             TdApi.Message message = update.message;
             long msgId = update.message.id;
             long chatId = update.message.chatId;
@@ -144,87 +140,21 @@ public final class Bot {
                     throw new RuntimeException(e);
                 }
             }
-            //if text is YouTube link
-            else if (matchesPattern(text, linkRegex) && senderUserId == USER_ID) {
-                Youtube youtube = new Youtube();
-                String id = matchesPattern(text, shortsRegex) ? youtube.getShortsId(text) : youtube.getVidId(text);
-                try {
-                    // Create a CompletableFuture<Boolean> to control the loop
-                    CompletableFuture<Boolean> isEnd = new CompletableFuture<>();
-
-                    // Asynchronously send the video message
-                    CompletableFuture<Void> videoCompletionFuture = CompletableFuture.runAsync(() -> {
-                        try {
-                            sendVideoMessage(message, youtube.getUrl(id));
-                            // Set the result of the CompletableFuture to true when sendVideoMessage is completed
-                            isEnd.complete(true);
-                        } catch (IOException | InterruptedException e) {
-                            isEnd.complete(false);
-                            throw new RuntimeException(e);
-                        }
-                    });
-
-                    // While loop controlled by the isEnd CompletableFuture
-                    TdApi.InputMessageText editMessage = new TdApi.InputMessageText();
-                    while (!isEnd.isDone()) {
-                        // Update the "Please wait" message with different dots
-                        editMessage.text = new TdApi.FormattedText("Please wait\uD83D\uDD52", null);
-                        editMessage(chatId, msgId, editMessage);
-                        TimeUnit.MILLISECONDS.sleep(400);
-                        if (isEnd.isDone()) {
-                            break;
-                        }
-
-                        editMessage.text = new TdApi.FormattedText("Please wait\uD83D\uDD55", null);
-                        editMessage(chatId, msgId, editMessage);
-                        TimeUnit.MILLISECONDS.sleep(400);
-                        if (isEnd.isDone()) {
-                            break;
-                        }
-
-                        editMessage.text = new TdApi.FormattedText("Please wait\uD83D\uDD58", null);
-                        editMessage(chatId, msgId, editMessage);
-                        TimeUnit.MILLISECONDS.sleep(400);
-                        if (isEnd.isDone()) {
-                            break;
-                        }
-
-                        editMessage.text = new TdApi.FormattedText("Please wait\uD83D\uDD5B", null);
-                        editMessage(chatId, msgId, editMessage);
-                        TimeUnit.MILLISECONDS.sleep(400);
-                    }
-
-                    // Delete the "Please wait" message
-                    deleteMessage(chatId, msgId);
-
-                    // Wait for the videoCompletionFuture to complete
-                    videoCompletionFuture.join();
-
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            // Else check for YouTube Link
+            else {
+                Youtube.checkYoutubeURL(update);
             }
-            //If you want to animate text
 
         }
     }
-
-
-    //Video message
-    private static void sendVideoMessage(TdApi.Message message, String videoUrl) throws IOException {
-        Path downloadedVideoPath = downloadVideo(videoUrl);
-        sendFileMessage(message, downloadedVideoPath);
-    }
-
-    //Delete message
-    private static void deleteMessage(long chatId, long messageId) {
+    static void deleteMessage(long chatId, long messageId) {
         TdApi.DeleteMessages request = new TdApi.DeleteMessages(chatId, new long[]{messageId}, true);
         client.send(request, response -> {
         });
     }
 
     //Edit message
-    private static void editMessage(long chatId, long messageId, TdApi.InputMessageContent message) {
+    static void editMessage(long chatId, long messageId, TdApi.InputMessageContent message) {
         TdApi.EditMessageText request = new TdApi.EditMessageText(chatId, messageId, null, message);
         client.send(request, response -> {
         });
@@ -244,52 +174,6 @@ public final class Bot {
             editMessage(chatId, msgId, editMessage);
             TimeUnit.MILLISECONDS.sleep(time);
         }
-    }
-
-    private static Path downloadVideo(String videoUrl) throws IOException {
-        String filename = UUID.randomUUID().toString() + ".mp4";
-        Path downloadDirectory = Paths.get("downloaded_videos");
-        if (!Files.exists(downloadDirectory)) {
-            Files.createDirectories(downloadDirectory);
-        }
-        Path downloadedVideoPath = downloadDirectory.resolve(filename);
-        try (InputStream in = new URL(videoUrl).openStream()) {
-            Files.copy(in, downloadedVideoPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-        return downloadedVideoPath;
-    }
-
-    private static void sendFileMessage(TdApi.Message message, Path filePath) throws IOException {
-
-        // Now send the actual video
-        TdApi.InputMessageVideo inputVideo = new TdApi.InputMessageVideo();
-        inputVideo.video = new TdApi.InputFileLocal(filePath.toString());
-
-        TdApi.SendMessage videoMessageRequest = new TdApi.SendMessage();
-        videoMessageRequest.replyToMessageId = message.replyToMessageId;
-        videoMessageRequest.chatId = message.chatId;
-        videoMessageRequest.inputMessageContent = inputVideo;
-        client.send(videoMessageRequest, videoResponse -> {
-            // Handle the response if needed
-        });
-
-        // Schedule the file deletion
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.schedule(() -> {
-            try {
-                Files.deleteIfExists(filePath);
-                System.out.println("File deleted: " + filePath);
-            } catch (IOException e) {
-                System.out.println("Can't delete");
-            }
-        }, 60, TimeUnit.SECONDS);
-
-    }
-
-    public static boolean matchesPattern(String text, String pattern) {
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(text);
-        return matcher.matches();
     }
 
     private static TdApi.PhotoSize getLargestPhotoSize(TdApi.Photo photo) {
